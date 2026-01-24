893ed6c53743909c8ff6b96ef7a9be35ea118299
Krzysztof Walo <krzysztof.walo@gmail.com>
2026-01-24 20:23:34 +0100

    Update hide vacant tags patch

--- dwm.c
+++ dwm.c
@@ -455,7 +455,7 @@ buttonpress(XEvent *e)
 	if (ev->window == selmon->barwin) {
 		i = x = 0;
 		for (c = m->clients; c; c = c->next)
-			occ |= c->tags == 255 ? 0 : c->tags;
+			occ |= c->tags == TAGMASK ? 0 : c->tags;
 		do {
 			/* do not reserve space for vacant tags */
 			if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
@@ -756,7 +756,7 @@ drawbar(Monitor *m)
 	}
 
 	for (c = m->clients; c; c = c->next) {
-		occ |= c->tags == 255 ? 0 : c->tags;
+		occ |= c->tags == TAGMASK ? 0 : c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
